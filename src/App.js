7d479e72b94f83ce38f9b0ca0c916ceeb5aeabae
Avinash Kumar Singh <avkumars@adobe.com>
2021-05-05 03:00:46 +0530

    It should fail

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Warning = React.lazy(() => import('./warning'));
 
-const a = "asasasasasasgit"
+const a = "asassgit"
 
 class App extends React.Component {
   state = {
